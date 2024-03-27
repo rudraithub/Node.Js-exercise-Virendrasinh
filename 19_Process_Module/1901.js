cea06ec90222f29c5523d0913b09bba33486dc2f
virendrasinh2240 <pvirendrasinh376@gmail.com>
2024-03-27 15:16:26 +0530

    done

---
+++ 19_Process_Module/1901.js
@@ -0,0 +1,35 @@
+// ## 1901 - Write a program that utilizes various methods and properties provided by the process module
+
+
+// let process = require("process")
+// let a = process.arch
+// console.log(a)
+
+// let b = process.env
+// console.log(b)
+
+// let c  = process.pid
+// console.log(c)
+
+// let d = process.platform
+// console.log(d)
+
+// let e = process.release
+// console.log(e)
+
+// let f = process.version
+// console.log(f)
+
+// let g = process.versions
+// console.log(g)
+
+// let h = process.cwd()
+// console.log(h)
+
+// let i = process.hrtime()
+// console.log(i)
+
+// let j = process.memoryUsage()
+// console.log(j)
+// let k = process.uptime()
+// console.log(k)
